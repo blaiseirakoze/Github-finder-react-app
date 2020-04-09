138b57f30133bf122266eff2040cd295cfd413b2
BlaiseJavan <blaiseirakoze50@gmail.com>
2020-04-09 16:39:33 +0200

    featch all users

--- src/components/user/Useritem.js
+++ src/components/user/Useritem.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react'
 
 class Useritem extends Component {
-    constructor() {
-        super()
-        this.state = {
-            id: 1,
-            login: 'mojombo',
-            avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-            html_url: 'https://api.github.com/users/mojombo'
-        }
-    }
 
     render() {
+        const { login, avatar_url, html_url } = this.props.user
+
         return (
-            <div className='card text-center'>
+            <div className='card text-center' style={{ alignItems: 'center' }}>
                 <img
-                    src={this.state.avatar_url}
+                    src={avatar_url}
                     alt=''
                     className='round-img'
                     style={{ width: '60px' }}
                 />
+                <h3>{login}</h3>
+                <div>
+                    <a href={html_url} className="btn btn-dark btn-sm my-1">
+                        More
+                    </a>
+                </div>
             </div>
         )
     }
